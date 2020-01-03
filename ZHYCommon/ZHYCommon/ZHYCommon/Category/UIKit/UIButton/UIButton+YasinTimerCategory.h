14264aaa740c99c0eb847f023cf9c840a5d78f6c
zhouhouyun <562475163@qq.com>
2020-01-03 14:22:19 +0800

    1.倒计时按钮分类

--- ZHYCommon/ZHYCommon/ZHYCommon/Category/UIKit/UIButton/UIButton+YasinTimerCategory.h
+++ ZHYCommon/ZHYCommon/ZHYCommon/Category/UIKit/UIButton/UIButton+YasinTimerCategory.h
@@ -8,6 +8,16 @@
 
 #import <UIKit/UIKit.h>
 
+//倒计时(如果按钮倒计时出现闪烁，将xib或者storyboard中Type属性设置为custom即可)
+//[_vcodeButton startCountDownTime:60 withCountDownBlock:^{
+//
+//    NSLog(@"开始倒计时");
+//
+//    //此处发送验证码等操作
+//    //................
+//
+//}];
+
 @interface UIButton (YasinTimerCategory)
 
 - (void)startCountDownTime:(int)time withCountDownBlock:(void(^)(void))countDownBlock;
